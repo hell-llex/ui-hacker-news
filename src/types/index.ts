3bb59c774a271ebaf3245b5c0faa03ac972b70e2
hell-llex <demeshenko333@gmail.com>
2024-05-03 19:07:29 +0300

    feat: add types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,28 @@
 export interface News {
-  
+  id: number;
+  deleted?: boolean;
+  type: 'job' | 'story' | 'comment' | 'poll' | 'pollopt';
+  by: string;
+  time: number; 
+  text?: string;
+  dead?: boolean;
+  parent?: number;
+  poll?: number; 
+  kids?: number[]; 
+  url: string; 
+  score: number; 
+  title: string; 
+  parts?: number[]; 
+  descendants?: number; 
+}
+export interface IComment {
+  by: string;
+  id: number;
+  kids?: number[];
+  parent?: number;
+  text: string;
+  time: number;
+  type: string;
+  children?: IComment[];
 }
 
